2af835139c88ab4c206fdb94a3569418630d40a1
Luca Casonato <lucacasonato@yahoo.com>
2020-08-23 00:42:12 +0200

    Bump to 1.3.1

--- deps.ts
+++ deps.ts
@@ -4,9 +4,8 @@ export {
   expandGlob,
   walk,
   readJsonSync,
-} from "https://deno.land/std@0.63.0/fs/mod.ts";
-export { join } from "https://deno.land/std@0.63.0/path/mod.ts";
-export { S3Bucket } from "https://raw.githubusercontent.com/lucacasonato/deno_s3/fa75e7a745c6856ef03add70a8cb37fb37e818ce/mod.ts";
+} from "https://deno.land/std@0.66.0/fs/mod.ts";
+export { join } from "https://deno.land/std@0.66.0/path/mod.ts";
 export type {
   APIGatewayProxyEventV2,
   APIGatewayProxyResultV2,
@@ -18,5 +17,6 @@ export {
   MongoClient,
   ObjectId,
 } from "https://raw.githubusercontent.com/lucacasonato/deno_mongo_lambda/v0.10.0/mod.ts";
+export { S3Bucket } from "https://raw.githubusercontent.com/lucacasonato/deno_s3/fa75e7a745c6856ef03add70a8cb37fb37e818ce/mod.ts";
 export { SQSQueue } from "https://raw.githubusercontent.com/lucacasonato/deno_sqs/6685e2a534122c01e3bfacbe6e590b8e5931633f/mod.ts";
 export { lookup } from "https://deno.land/x/media_types@v2.4.3/mod.ts";
